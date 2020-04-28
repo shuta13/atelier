3408bf7bf91a715f10a867f127590a5161fe5c0e
shuta13 <hollystarsun@gmail.com>
2020-04-28 15:22:19 +0900

    wip: 013

---
+++ pages/learn/013/index.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState, useCallback, useRef } from "react";
+import MyGLSL from "../../../components/common/MyGLSL";
+
+const frag = require("./shader/013.frag");
+
+const _013: React.FC = () => {
+  const [uTime, setUTime] = useState(0.0);
+  const requestRef = useRef(0.0);
+  const animate = useCallback(() => {
+    setUTime(performance.now() * 0.001)
+    requestRef.current = requestAnimationFrame(animate)
+  }, [])
+  useEffect(() => {
+    requestRef.current = requestAnimationFrame(animate)
+    return () => cancelAnimationFrame(requestRef.current)
+  }, [animate])
+  return <MyGLSL frag={frag.default} vert="" uniforms={{ 
+    u_resolution: [400, 400],
+    // u_mouse: [x, y],
+    u_time: uTime
+  }} />;
+};
+
+export default _013;
