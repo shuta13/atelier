a1f88422eb955c55237815cda6f54b72330cfde7
shuta13 <hollystarsun@gmail.com>
2020-04-29 12:45:50 +0900

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,12 +8,11 @@ import { Header } from "../components/common/Header";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const router = useRouter();
-  const pathname = router.pathname;
-  console.log(pathname);
+  const ogImageName = router.pathname === "/" ? "/og-image" : router.pathname;
   return (
     <>
       <Head>
-        <meta property="og:image" content={`https://learn-webgl.now.sh${pathname}.png`} />
+        <meta property="og:image" content={`https://learn-webgl.now.sh${ogImageName}.png`} />
         <title>did0es - Learn WebGL</title>
       </Head>
       <Header />
